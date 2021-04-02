db0258b1f51f92d76cf220f5263db0b138a2d70c
susird <susir95@gmail.com>
2021-04-02 18:36:39 -0500

    First commit, introduction to challenge

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+//Declaring different variables of different data types
+let firstName = 'Asabehen' //first name of a person
+let lastName = 'Yetayeh' // last name of a person
+let country = 'Finland' // country
+let city = 'Helsinki' // capital country
+let age = 100 // age in years
+let isMarried = 'true'
+console.log(firstName, lastName, country, city, age, isMarried)
+
+// Declaring variables with number values
+const gravity = 9.81 // earth gravity in m/s2
+const boilingPoint = 100 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constant
+console.log(gravity, boilingPoint, PI)
+
+//Variables can also be declaring in one line separated by comma
+let name = 'Asabeneh',
+  job = 'teacher',
+  live = 'Finland'
+console.log(name, job, live)
+
+let myAge = 'I am 26 years old.'
+let yourAge = 'You are 30 years old.'
+console.log(myAge, yourAge)
