26ec80c5275db2221b54e07fbf34edbffc35ff8c
susird <susir95@gmail.com>
2021-04-12 21:35:44 -0500

    finishing 3th day

--- Day3/excercises_day3.js
+++ Day3/excercises_day3.js
@@ -118,8 +118,8 @@ function rectangle() {
   let length = prompt('Insert length here',);
   let width = prompt('Insert width here',);
   let recArea = (length * width);
-  let recperimeter = ((2) * (length + width))
-  alert(`The area of the rectangle is  ${recArea}`)
+  let recperimeter = ((2) * (length + width));
+  alert(`The area of the rectangle is  ${recArea}`);
   alert(`The perimeter of the rectangle is ${recperimeter}`)
 }
 //console.log(rectangle())
@@ -131,25 +131,125 @@ function circleArea() {
   let circRadius = prompt('Insert radius here',);
   let circArea = (Math.PI * circRadius * circRadius);
   let circumf = ((2) * Math.PI * circRadius);
-  alert(`The area of the circle is  ${circArea}`)
+  alert(`The area of the circle is  ${circArea}`);
   alert(`The circumference of the circle is  ${circumf}`)
 }
 //console.log(circleArea())
 
 //5.Calculate the slope, x-intercept and y-intercept of y = 2x -2
 function slopeCalculate() {
-  let x= prompt('Insert the intercept here', );
-  let y = ((2 * x) - 2)
+  let x = prompt('Insert the intercept here',);
+  let y = ((2 * x) - 2);
   alert(`The slope is  ${y}/${x}`)
 }
 //console.log(slopeCalculate())
 
 //6.Slope is (m = y2-y1/x2-x1). Find the slope between point (2, 2) and point(6,10)
 function calcSlope(coordinates) {
-  const {x1,x2,y1,y2} = coordinates;
+  const { x1, x2, y1, y2 } = coordinates;
   return (y2 - y1) / (x2 - x1);
 }
-console.log(calcSlope({x1: 2, y1: 2, x2: 6, y2: 10}))
+//console.log(calcSlope({x1: 2, y1: 2, x2: 6, y2: 10}))
 
 //7.Compare the slope of above two questions.
-console.log(slopeCalculate >= calcSlope)
+//console.log(slopeCalculate >= calcSlope)
+
+
+
+//8. Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is 0
+function calculateY() {
+  let range = [-8, -7, -6, -5, -6, -5, -4, -3, -2, -1, 0];
+  return range.map(x => Math.pow(x, 2) + 6 * x + 9)
+}
+console.log(calculateY())
+
+//9. Writ a script that prompt a user to enter hours and rate per hour. Calculate pay of the person?
+function pay() {
+  let hours = prompt('Insert hours here',);
+  let rate = prompt(`Insert rate per hour here`,);
+  let payment = (hours * rate);
+  alert(`Your weekly earning is ${payment}`)
+}
+//console.log(pay())
+
+//10.If the length of your name is greater than 7 say, your name is long else say your name is short.
+function sizeLength() {
+  let name = prompt('Insert your name here',);
+  if (name.length > 7) {
+    alert(`Your name is long`);
+  } else {
+    alert(`Your name is short`);
+  }
+}
+//console.log(sizeLength())
+
+//11. Compare your first name length and your family name length and you should get this output.
+function compareLength() {
+  let yourName = prompt('Insert your name here',);
+  let familyName = prompt('Insert your family name here',);
+  if (yourName.length > familyName.length) {
+    alert(`Your firs name, ${yourName} is longer than your family name, ${familyName}`);
+  } else {
+    alert(`Your family name, ${familyName} is longer than your firs name, ${yourName}`);
+  }
+}
+//console.log(compareLength())
+
+//12. Declare two variables myAge and yourAge and assign them initial values and myAge and yourAge.
+function compareAges() {
+  let myAge = 250;
+  let yourAge = prompt('Insert your age here',);
+  if (myAge > yourAge) {
+    alert(`I am ${myAge - yourAge} years older than you.`);
+  }
+}
+//console.log(compareAges())
+
+/*13.Using prompt get the year the user was born and if the user is 18 or above allow
+the user to drive if not tell the user to wait a certain amount of years.*/
+function driveAge() {
+  let birthYear = prompt('Insert your birth year here',);
+  let today = 2021;
+  let ofAge = 2003;
+  let yearsOfAge = birthYear - ofAge;
+  let age = today - birthYear;
+  if (birthYear < 18) {
+    alert(`You will be allowed to drive after ${yearsOfAge} years`);
+  } else {
+    alert(`You are ${age} old enough to drive`);
+  }
+}
+//console.log(driveAge())
+
+/*14.Write a script that prompt the user to enter number of years. Calculate the number 
+of seconds a person can live. Assume some one lives just hundred years*/
+function secondsLive () {
+  let birthYear = prompt('Enter number of yours you live',);
+  let seccondsYear = 31536000
+  let calculate = birthYear * seccondsYear
+  alert(`You lived ${calculate} seconds.`);
+}
+//console.log(secondsLive())
+
+//15. Create a human readable time format using the Date time object
+//. i 
+let todayYear = now.getFullYear()
+let month = now.getMonth() + 1
+let day = now.getDay()
+let hours = now.getHours()
+let minutes = now.getMinutes()
+console.log(`${todayYear}-${month}-${day} ${hours}:${minutes}`)
+//ii. 
+console.log(`${day}-${month}-${todayYear} ${hours}:${minutes}`)
+//iii.
+console.log(`${day}/${month}/${todayYear} ${hours}:${minutes}`)
+
+/*Create a human readable time format using the Date time object. The hour and the minute should be all
+ the time two digits(7 hours should be 07 and 5 minutes should be 05 )*/ 
+let dateYear = now.getFullYear()
+let dateMonth = now.getMonth() + 1
+let dateDay =  now.getDay()
+let d = new Date("2015-03-25T12:00:00-06:30");
+let dateHours = d.getHours()
+let dateMinutes = d.getMinutes()
+console.log(`${dateYear}-${dateMonth}-${dateDay} ${dateHours}:${dateMinutes}`)
