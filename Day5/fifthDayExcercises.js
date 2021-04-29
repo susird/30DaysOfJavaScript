df56e2586c4353b00e69297984f11b61bba111d8
susird <susir95@gmail.com>
2021-04-29 16:45:21 -0500

    solvin level 2 and 14th of level 1

--- Day5/fifthDayExcercises.js
+++ Day5/fifthDayExcercises.js
@@ -1,10 +1,11 @@
+
 // 30 Days Of JavaScript: Arrays
 //LEVEL 1
 
 //1. Declare an empty array
 const emptyArray = Array();
 //2. Declare an array with more than 5 number of elements
-const  numbersList = [5, 4, 8, 2, 9, 3, 7];
+const numbersList = [5, 4, 8, 2, 9, 3, 7];
 
 //3.Find the length of your array
 console.log(numbersList.length);
@@ -12,7 +13,7 @@ console.log(numbersList.length);
 //4. Get the first item, the middle item and the last item of the array
 const FIRST = 0
 console.log(numbersList[FIRST]);
-let lastNumber = numbersList.length -1;
+let lastNumber = numbersList.length - 1;
 lastNumber = numbersList[lastNumber];
 console.log(lastNumber)
 const middleNumber = numbersList[Math.floor(numbersList.length / 2)];
@@ -38,7 +39,7 @@ console.log(itCompanies.length);
 console.log(itCompanies[FIRST]);
 const middleCompany = itCompanies[Math.floor(itCompanies.length / 2)];
 console.log(middleCompany)
-let lastCompany = itCompanies.length -1;
+let lastCompany = itCompanies.length - 1;
 lastCompany = itCompanies[lastCompany];
 console.log(lastCompany)
 
@@ -46,7 +47,7 @@ console.log(lastCompany)
 itCompanies.forEach(element => console.log(element))
 
 //11. Change each company name to uppercase one by one and print them out
-itCompanies.map(function(x) {return console.log(x.toUpperCase());})
+itCompanies.map(function (x) { return console.log(x.toUpperCase()); })
 
 //12.Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies. 
 console.log(itCompanies.toString() + ' are big IT companies')
@@ -55,17 +56,19 @@ console.log(itCompanies.toString() + ' are big IT companies')
 If it exist return the company else return a company is not found*/
 const IT_EXIST = 0;
 const companyExist = itCompanies.indexOf('Facebook')
-if ( companyExist === IT_EXIST) {
+if (companyExist === IT_EXIST) {
   console.log('This company does exist')
 } else {
   console.log('Company is not found')
 }
 
 //14. Filter out companies which have more than one 'o' without the filter method
-let aCountArr = itCompanies.map(i => {
-  return i.match(/o/g).length;
+itCompanies.forEach(element => {
+  const aCountArray = element.replace(/[^o]/g, "").length;
+  if (aCountArray > 1) {
+    console.log(element)
+  }
 });
-console.log(aCountArr);
 
 //15. Sort the array using sort() method
 console.log(itCompanies.sort())
@@ -74,13 +77,13 @@ console.log(itCompanies.sort())
 console.log(itCompanies.reverse())
 
 //17. Slice out the first 3 companies from the array
-console.log(itCompanies.slice(0,3))
+console.log(itCompanies.slice(0, 3))
 
 //18. Slice out the last 3 companies from the array
 console.log(itCompanies.slice(4))
 
 //19. Slice out the middle IT company or companies from the array
-console.log(itCompanies.slice(3,4))
+console.log(itCompanies.slice(3, 4))
 
 //20.Remove the first IT company from the array
 const facebookleIndex = itCompanies.indexOf('Facebook');
@@ -98,3 +101,12 @@ console.log(itCompanies.splice(amazonIndex, 1));
 console.log(itCompanies.splice())
 
 //LEVEL 2
+/*1.Create a separate countries.js file and store the countries array in to this file,
+ create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file*/
+const countries = require("./countries.js");
+console.log(countries)
+
+const webTechs = require("./web_techs.js");
+console.log(webTechs)
+
+//2. First remove all the punctuations and change the string to array and count the number of words in the array
