e95ed8e4c6e6e716f5ffe456b5fecaf19af4e0c1
susird <susir95@gmail.com>
2021-05-02 21:19:53 -0500

    solve exccercises level 3

--- Day5/fifthDayExcercises.js
+++ Day5/fifthDayExcercises.js
@@ -1,5 +1,5 @@
+const webTechs = require("./web_techs.js");
 const countries = require("./countries.js");
-//console.log(countries)
 
 // // 30 Days Of JavaScript: Arrays 
 // //LEVEL 1
@@ -106,7 +106,7 @@ const countries = require("./countries.js");
 // /*1.Create a separate countries.js file and store the countries array in to this file,
 //  create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file*/
 
-// const webTechs = require("./web_techs.js");
+// console.log(countries)
 // console.log(webTechs)
 
 // //2. First remove all the punctuations and change the string to array and count the number of words in the array
@@ -133,13 +133,38 @@ const countries = require("./countries.js");
 // /*4. In countries array check if 'Ethiopia' exists in the array if it exists print 
 // 'ETHIOPIA'. If it does not exist add to the countries list.*/
 
-function findingCountry(countries = [], country = '') {
-  const isEthiopia = countries.indexOf(country) !== -1
-  if (isEthiopia)  {
-    console.log(country);
-  } else {
-    const mergedArrays = countries.concat([country]);
-    console.log(mergedArrays) 
-  }
-}
-findingCountry(countries, 'Ethiopia');
+// function findingCountry(countries = [], country = '') {
+//   const isEthiopia = countries.indexOf(country) !== -1;
+//   if (isEthiopia)  {
+//     console.log(country);
+//   } else {
+//     const mergedArrays = countries.concat([country]);
+//     console.log(mergedArrays) 
+//   }
+// }
+// findingCountry(countries, 'Ethiopia');
+
+/*5.In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a
+CSS preprocess'. If it does not exist add Sass to the array and print the array.*/
+// function findingWebTech(webTech, tech) {
+//   const hasSass = webTech.indexOf(tech) !== -1;
+//   if (hasSass) {
+//     console.log(`${tech} is a CSS preprocess`);
+//   }else {
+//     const mergedTech = webTechs.concat([tech]);
+//     console.log(mergedTech)
+//   }
+// }
+// findingWebTech(webTechs, 'Sass');
+
+//6.Concatenate the following two variables and store it in a fullStack variable.
+// const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+// const backEnd = ['Node', 'Express', 'MongoDB'];
+// const fullStack = frontEnd.concat(backEnd)
+// console.log(fullStack)
+
+//LEVEL 3
+//1. The following is an array of 10 students ages:
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+  // Sort the array and find the min and max age
+  
