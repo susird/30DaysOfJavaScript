548bee07ee9d70f5f0b67571bd4911380e767396
susird <susir95@gmail.com>
2021-05-27 17:58:43 -0500

    converting into function first exercise

--- Day6/6dayExecises.js
+++ Day6/6dayExecises.js
@@ -1,10 +1,34 @@
-// //LOOPS
+//LOOPS
 
-// //Level 1
-// //1.Iterate 0 to 10 using for loop, do the same using while and do while loop
-// for(let i = 0;i <= 10; i++) {
-//   console.log(i);
-// }
+//Level 1
+//1.Iterate 0 to 10 using for loop, do the same using while and do while loop
+//for loop
+function iterateNumbers (start, finish) {
+  for (let i = start; i <= finish; i++) {
+    console.log(i);
+  }
+}
+iterateNumbers(0, 10);
+
+//while
+function iterateWithWhile(start, finish) {
+  let i = start
+  while (i <= finish) {
+    console.log(i);
+    i++;
+  }
+}
+iterateWithWhile(0, 10);
+
+// while loop
+function iterateWithWhileLoop(start, finish) {
+  let i = start
+  do {
+    console.log(i);
+    i++;
+  } while (i <= finish);
+}
+iterateWithWhileLoop(0, 10);
 
 // let i = 0
 // while (i <= 10) {
