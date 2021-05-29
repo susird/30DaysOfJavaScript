61bc9ba0370c5119f827bf5963c918570078a65a
susird <susir95@gmail.com>
2021-05-28 21:11:24 -0500

    doing exercises into functions

--- Day6/6dayExecises.js
+++ Day6/6dayExecises.js
@@ -3,7 +3,7 @@
 //Level 1
 //1.Iterate 0 to 10 using for loop, do the same using while and do while loop
 //for loop
-function iterateNumbers (start, finish) {
+function iterateNumbers(start, finish) {
   for (let i = start; i <= finish; i++) {
     console.log(i);
   }
@@ -12,7 +12,7 @@ iterateNumbers(0, 10);
 
 //while
 function iterateWithWhile(start, finish) {
-  let i = start
+  let i = start;
   while (i <= finish) {
     console.log(i);
     i++;
@@ -22,7 +22,7 @@ iterateWithWhile(0, 10);
 
 // while loop
 function iterateWithWhileLoop(start, finish) {
-  let i = start
+  let i = start;
   do {
     console.log(i);
     i++;
@@ -30,39 +30,42 @@ function iterateWithWhileLoop(start, finish) {
 }
 iterateWithWhileLoop(0, 10);
 
-// let i = 0
-// while (i <= 10) {
-//   console.log(i);
-//   i++;
-// }
-
-// let i = 0
-// do {
-//   console.log(i);
-//   i++;
-// } while ( i <= 10);
-
 // //2. Iterate 10 to 0 using for loop, do the same using while and do while loop
-// for (let i = 10; i >= 0; i--) {
-//   console.log(i);
-// }
+// for loop
+function iterateToLowest(highest, lowest) {
+  for (let i = highest; i >= lowest; i--) {
+    console.log(i);
+  }
+}
+iterateToLowest(10, 0);
 
-// let i = 10
-// while (i >= 0) {
-//   console.log(i);
-//   i--;
-// }
+// while
+function iterateToLowestWhile(highest, lowest) {
+  let i = highest;
+  while (i >= lowest) {
+    console.log(i);
+    i--;
+  }
+}
+iterateToLowestWhile(10, 0);
 
-// let i = 10
-// do {
-//   console.log(i);
-//   i--;
-// } while ( i >= 0);
+//while loop
+function iterateToLowestWhileLoop(highest, lowest) {
+  let i = highest;
+  do {
+    console.log(i);
+    i--;
+  } while (i >= lowest);
+}
+iterateToLowestWhileLoop(10, 0);
 
 // //3.Iterate 0 to n using for loop
-// for(let n = 0; n <= 5; n++) {
-//   console.log(n);
-// }
+function iterateToN(start, nNum) {
+  for (let i = start; i <= nNum; i++) {
+    console.log(i);
+  }
+}
+iterateToN(0, 30);
 
 // /*4. Write a loop that makes the following pattern using console.log():
 //     #
@@ -73,15 +76,18 @@ iterateWithWhileLoop(0, 10);
 //     ######
 //     #######
 // */
-// let n = 7;
-// let stringPattern = '';
-// for (let i = 1; i <= n; i++) {
-//   for (let a = 0; a < i; a++) {
-//     stringPattern += '#';
-//   }
-//   stringPattern += '\n';
-// }
-// console.log(stringPattern)
+function makePattern(start, finish, simbol) {
+  let lastNumber = finish;
+  let stringPattern = '';
+  for (let i = start; i <= lastNumber; i++) {
+    for (let a = 0; a < i; a++) {
+      stringPattern += simbol;
+    }
+    stringPattern += '\n';
+  }
+  console.log(stringPattern);
+}
+makePattern(1, 7, '#');
 
 // /*5.Use loop to print the following pattern:
 // 0 x 0 = 0
@@ -96,9 +102,12 @@ iterateWithWhileLoop(0, 10);
 // 9 x 9 = 81
 // 10 x 10 = 100
 // */
-// for (let i = 0; i <= 10; i++) {
-//   console.log(`${i} * ${i} = ${i * i}`)
-// }
+function printMultiplication (start, end) {
+  for (let i = start; i <= end; i++) {
+    console.log(`${i} * ${i} = ${i * i}`);
+  }
+}
+printMultiplication(0, 10);
 
 /*6. Using loop print the following pattern
  i    i^2   i^3
@@ -114,6 +123,9 @@ iterateWithWhileLoop(0, 10);
  9    81    729
  10   100   1000
 */
-for (let i = 0; i <= 10; i++) {
-  console.log(`${i}  ${i * i} ${i * i * i}`)
+function printMultiplication (start, end) {
+  for (let i = start; i <= end; i++) {
+    console.log(`${i}  ${i * i}  ${i * i * i}`);
+  }
 }
+printMultiplication(0, 10);
