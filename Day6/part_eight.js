61942ce13131128b6f9bf0c2a62b7a252a658488
Susana Ruiz <susanaruiz@Susanas-MacBook-Air.local>
2021-06-18 15:27:48 -0500

    changing function name

--- Day6/part_eight.js
+++ Day6/part_eight.js
@@ -1,9 +1,9 @@
 //8.Use for loop to iterate from 0 to 100 and print only odd numbers
-function iterateOddNumbers(start, finish) {
+function printOddNumbers(start, finish) {
   for (let i = start; i <= finish; i++) {
     const isOdd = i % 2 !== 0;
     if (isOdd)
       console.log(i);
   }
 }
-iterateOddNumbers(0, 100);
+printOddNumbers(0, 100);
