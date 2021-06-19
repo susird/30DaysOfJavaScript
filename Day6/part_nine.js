5acc8ec515270ecf2c3ab8aa7066857f44814b3f
Susana Ruiz <susanaruiz@Susanas-MacBook-Air.local>
2021-06-19 14:54:31 -0500

    fixing variable names and params of the function

--- Day6/part_nine.js
+++ Day6/part_nine.js
@@ -1,15 +1,14 @@
 //9.Use for loop to iterate from 0 to 100 and print only prime numbers
-function printPrimeNumbers() {
+function printPrimeNumbers(start, end) {
   function validateIfIsPrime(num) {
-    let sumZero = 0;
+    let counterPrimeNumbers = 0;
     for (let i = num; i > 0; i--) {
       if (num % i === 0) {
-        sumZero++;
+        counterPrimeNumbers++;
       }
     }
-    return sumZero === 2 && num > 1;
+    return counterPrimeNumbers  === 2 && num > 1;
   }
-  const [start, end] = [0, 100];
   for (let i = start; i < end; i++) {
     if (validateIfIsPrime(i)) {
       console.log(i);
