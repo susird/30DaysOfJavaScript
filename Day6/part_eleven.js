e8de263fa4d14b6da52e725913480e2148d7b5c5
susana <susir95@gmail.com>
2021-06-23 14:38:16 -0500

    fixing function name

--- Day6/part_eleven.js
+++ Day6/part_eleven.js
@@ -1,5 +1,5 @@
 //11. Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
-function iterateOddNumbers(start, end) {
+function printOddAndEvenNumbers(start, end) {
   let oddCounter = 0;
   let evenCounter = 0;
   for (let i = start; i <= end; i++) {
